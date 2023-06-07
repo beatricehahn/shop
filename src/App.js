640bb9a7298294776452a069f11d35d1591a4bbe
Beatrice Hahn <beatricehahn7@gmail.com>
2023-06-07 12:43:05 -0700

    Begin building out navigation bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-import { Outlet } from 'react-router-dom';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am a navigator</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
+import SignIn from './routes/signIn/signIn.component';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index  element={<Home />} />
+        <Route path='sign-in'  element={<SignIn />} />
       </Route>
     </Routes>
   );
