b34d60fccda454ee4cdc9e62ebd00a78f918c654
Beatrice Hahn <beatricehahn7@gmail.com>
2023-06-07 11:36:49 -0700

    Add Outlet from react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import { Outlet } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am a navigator</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index  element={<Home />} />
+      </Route>
     </Routes>
   );
 };
