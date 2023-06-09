7b5c2c2b36507a99bc07ab768af37b38d9226a92
Beatrice Hahn <beatricehahn7@gmail.com>
2023-06-09 15:31:11 -0700

    Render sign out with context

--- src/components/sign-in-form/signInForm.component.jsx
+++ src/components/sign-in-form/signInForm.component.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import { 
     signInAuthUserWithEmailAndPassword, 
     createUserDocFromAuth,
     signInWithGooglePopup
 } from '../../utils/firebase/firebase.utils';
+
+import { UserContext } from '../../contexts/user.context';
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
@@ -20,6 +22,8 @@ const defaultFormFields = {
 const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
+
+    const { setCurrentUser } = useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -39,7 +43,7 @@ const SignInForm = () => {
                 email,
                 password
             );
-            console.log(response);
+
             resetFormFields();
         } catch (error) {
             switch (error.code) {
--- src/components/sign-up-form/signupForm.component.jsx
+++ src/components/sign-up-form/signupForm.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import { 
     createAuthUserWithEmailAndPassword, 
@@ -7,6 +7,8 @@ import {
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
+
+import { UserContext } from '../../contexts/user.context';
 
 import './signUpForm.styles.scss';
 
@@ -21,6 +23,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
+
+    const { setCurrentUser } = useContext(UserContext); 
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -41,6 +45,8 @@ const SignUpForm = () => {
                 email,
                 password
             );
+
+            setCurrentUser(user);
 
             await createUserDocFromAuth(user, { displayName });
             resetFormFields();
